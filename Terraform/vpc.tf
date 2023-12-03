50451ba6d13538176be9ac64d13a573e0af6a974
Ranieri <ranieri.imperatori@gmail.com>
2023-12-03 12:36:09 -0300

    fixing terraform folders

--- Terraform/vpc.tf
+++ Terraform/vpc.tf
@@ -6,13 +6,13 @@
  }
 
  module "vpc" {
-   source = "terraform-aws-modules/vpc/aws"
-   version = "5.0.0"
+   source          = "terraform-aws-modules/vpc/aws"
+   version         = "5.0.0"
 
-   name = "lakehouse-challenge-dev"
-   cidr = "10.0.0.0/16"
+   name            = "lakehouse-challenge-dev"
+   cidr            = "10.0.0.0/16"
 
-   azs = data.aws_availability_zones.available
+   azs             = data.aws_availability_zones.available.names
    private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-   public_subnets = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+   public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
  }
